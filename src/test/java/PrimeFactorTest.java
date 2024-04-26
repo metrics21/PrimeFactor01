5ec87d24c8e6fcc7739714de5977bb5344745e8c
metrics <metrics21@naver.com>
2024-04-26 17:00:45 +0900

    Metrics21 first Commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorTest {
+
+    @Test
+    public void testPrimeFactorOf01(){
+        PrimeFactor primefactor = new PrimeFactor();
+        assertEquals(10, 10);
+
+    }
+
+}
